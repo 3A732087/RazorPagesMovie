3577b720fb618e9f7d5b3ef740c8e934ecfd6b55
Quannn <quannn0180716v@gmail.com>
2023-08-20 21:42:14 +0800

    新增欄位、驗證

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -6,16 +6,30 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; } = string.Empty;
 
         //使用者不需要在 [日期] 欄位中輸入時間資訊。只會顯示日期，不會顯示時間資訊。
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        //類型
-        public string Genre { get; set; } = string.Empty;
-        
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        //類型
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; } = string.Empty;
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; } = string.Empty;
     }
 }
