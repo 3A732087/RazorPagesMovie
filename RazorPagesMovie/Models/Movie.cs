412dccee857842bd45c2a7e3c234d3141d560f1e
Quannn <quannn0180716v@gmail.com>
2023-08-20 21:08:50 +0800

    Models屬性更新顯示名稱、搜尋功能新增

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -8,10 +9,13 @@ namespace RazorPagesMovie.Models
         public string Title { get; set; } = string.Empty;
 
         //使用者不需要在 [日期] 欄位中輸入時間資訊。只會顯示日期，不會顯示時間資訊。
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         //類型
-        public string Genre { get; set; } = string.Empty;   
+        public string Genre { get; set; } = string.Empty;
+        
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
